6c9fcd987a43b675ae09704540e4c475d6e1e33e
guille <guillermovallejolopez@gmail.com>
2022-05-09 12:56:37 +0200

    Perfil y editar perfil

--- src/principal/editarPerfil.java
+++ src/principal/editarPerfil.java
@@ -16,12 +16,10 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
 import javax.swing.border.CompoundBorder;
 
-public class Perfil {
+public class editarPerfil {
 
 	private JFrame frame;
 	private JPanel header;
-	private JLabel lblNewLabel_1;
-	private JLabel lblNewLabel_2;
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_3;
 	private JLabel lblNewLabel_4;
@@ -29,16 +27,17 @@ public class Perfil {
 	private JLabel lblNewLabel_6;
 	private JLabel lblNewLabel_7;
 	private JLabel lblNewLabel_8;
-	private JLabel lblNewLabel_9;
-	private JLabel lblNewLabel_10;
 	private JLabel lblNewLabel_11;
-	private JLabel lblNewLabel_12;
 	private JLabel lblNewLabel_13;
 	private JLabel lblNewLabel_14;
-	private JTextArea txtrFdsfsd;
 	private JButton btnNewButton_1;
 	private JButton btnNewButton_2;
-	private JButton btnNewButton_3;
+	private JTextField txtNuevaLocalidad;
+	private JTextField txtNuevoSexo;
+	private JTextField txtNuevoDeporte;
+	private JTextArea txtrModificarDescripcion;
+	private JTextField txtNuevoNombreDe;
+	private JTextField txtNuevoNombre;
 
 	/**
 	 * Launch the application.
@@ -47,7 +46,7 @@ public class Perfil {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Perfil window = new Perfil();
+					editarPerfil window = new editarPerfil();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -59,7 +58,7 @@ public class Perfil {
 	/**
 	 * Create the application.
 	 */
-	public Perfil() {
+	public editarPerfil() {
 		initialize();
 	}
 
@@ -80,21 +79,9 @@ public class Perfil {
 		frame.getContentPane().add(header);
 		header.setLayout(null);
 		
-		lblNewLabel_1 = new JLabel("@DogueDrake");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(618, 28, 123, 28);
-		header.add(lblNewLabel_1);
-		
-		lblNewLabel_2 = new JLabel("Luis Rodriguez Gonzalez");
-		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_2.setBounds(547, 52, 194, 28);
-		header.add(lblNewLabel_2);
-		
 		lblNewLabel = new JLabel("");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/appppp-modified.png")));
+		lblNewLabel.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/image-modified-modified.png")));
 		lblNewLabel.setBounds(736, 0, 101, 100);
 		header.add(lblNewLabel);
 		
@@ -105,9 +92,25 @@ public class Perfil {
 			}
 		});
 		btnNewButton_1.setBackground(new Color(57, 62, 70));
-		btnNewButton_1.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/home-48.png")));
+		btnNewButton_1.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/home-48.png")));
 		btnNewButton_1.setBounds(10, 10, 85, 70);
 		header.add(btnNewButton_1);
+		
+		txtNuevoNombreDe = new JTextField();
+		txtNuevoNombreDe.setForeground(Color.LIGHT_GRAY);
+		txtNuevoNombreDe.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtNuevoNombreDe.setText("Nuevo nombre de usuario...");
+		txtNuevoNombreDe.setBounds(523, 27, 203, 23);
+		header.add(txtNuevoNombreDe);
+		txtNuevoNombreDe.setColumns(10);
+		
+		txtNuevoNombre = new JTextField();
+		txtNuevoNombre.setForeground(Color.LIGHT_GRAY);
+		txtNuevoNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtNuevoNombre.setText("Nuevo nombre...");
+		txtNuevoNombre.setBounds(523, 56, 203, 23);
+		header.add(txtNuevoNombre);
+		txtNuevoNombre.setColumns(10);
 		
 		JButton btnNewButton = new JButton("");
 		btnNewButton.setBorder(null);
@@ -116,64 +119,45 @@ public class Perfil {
 			}
 		});
 		btnNewButton.setBackground(new Color(34, 40, 49));
-		btnNewButton.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/arrow.png")));
+		btnNewButton.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/arrow.png")));
 		btnNewButton.setBounds(20, 99, 59, 52);
 		frame.getContentPane().add(btnNewButton);
 		
 		lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/marker-free-icon-font (1) (1) (1) (1).png")));
+		lblNewLabel_3.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/marker-free-icon-font (1) (1) (1) (1).png")));
 		lblNewLabel_3.setBounds(79, 129, 33, 33);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/venus-mars-free-icon-font (1).png")));
+		lblNewLabel_4.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/venus-mars-free-icon-font (1).png")));
 		lblNewLabel_4.setBounds(79, 172, 33, 33);
 		frame.getContentPane().add(lblNewLabel_4);
 		
 		lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/calendar-free-icon-font (1).png")));
+		lblNewLabel_5.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/calendar-free-icon-font (1).png")));
 		lblNewLabel_5.setBounds(299, 129, 33, 33);
 		frame.getContentPane().add(lblNewLabel_5);
 		
 		lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/basketball-free-icon-font (1).png")));
+		lblNewLabel_6.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/basketball-free-icon-font (1).png")));
 		lblNewLabel_6.setBounds(299, 172, 33, 33);
 		frame.getContentPane().add(lblNewLabel_6);
 		
 		lblNewLabel_7 = new JLabel("");
-		lblNewLabel_7.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/thumbs-up-free-icon-font (1).png")));
+		lblNewLabel_7.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/thumbs-up-free-icon-font (1).png")));
 		lblNewLabel_7.setBounds(505, 129, 33, 33);
 		frame.getContentPane().add(lblNewLabel_7);
 		
 		lblNewLabel_8 = new JLabel("");
-		lblNewLabel_8.setIcon(new ImageIcon(Perfil.class.getResource("/Imagenes/document-free-icon-font (1).png")));
+		lblNewLabel_8.setIcon(new ImageIcon(editarPerfil.class.getResource("/Imagenes/document-free-icon-font (1).png")));
 		lblNewLabel_8.setBounds(79, 215, 33, 33);
 		frame.getContentPane().add(lblNewLabel_8);
-		
-		lblNewLabel_9 = new JLabel("Eapa\u00F1a, Madid");
-		lblNewLabel_9.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_9.setForeground(Color.WHITE);
-		lblNewLabel_9.setBackground(Color.GREEN);
-		lblNewLabel_9.setBounds(111, 129, 145, 33);
-		frame.getContentPane().add(lblNewLabel_9);
-		
-		lblNewLabel_10 = new JLabel("Hombre");
-		lblNewLabel_10.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_10.setForeground(Color.WHITE);
-		lblNewLabel_10.setBounds(111, 172, 145, 33);
-		frame.getContentPane().add(lblNewLabel_10);
 		
 		lblNewLabel_11 = new JLabel("19 a\u00F1os");
 		lblNewLabel_11.setForeground(Color.WHITE);
 		lblNewLabel_11.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblNewLabel_11.setBounds(336, 129, 145, 33);
 		frame.getContentPane().add(lblNewLabel_11);
-		
-		lblNewLabel_12 = new JLabel("Padel, Baloncesto");
-		lblNewLabel_12.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_12.setForeground(Color.WHITE);
-		lblNewLabel_12.setBounds(336, 172, 145, 33);
-		frame.getContentPane().add(lblNewLabel_12);
 		
 		lblNewLabel_13 = new JLabel("125");
 		lblNewLabel_13.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -187,15 +171,7 @@ public class Perfil {
 		lblNewLabel_14.setBounds(111, 215, 145, 33);
 		frame.getContentPane().add(lblNewLabel_14);
 		
-		txtrFdsfsd = new JTextArea();
-		txtrFdsfsd.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtrFdsfsd.setForeground(Color.WHITE);
-		txtrFdsfsd.setText("Soy una persona un tanto egocentrica, me gusta la perfeccion en todos los ambitos posibles.En\r\ncuanto a los deprotes me considero un jugador con una deportividad ejemplar, no destaco\r\nen ningun deporte sinembargo, tento en baloncesto como en padel considero que estoy por\r\nencima de la media.\r\nMe uno a esta app para encontrar compa\u00F1eros con los que poder jugar partidos y porque \r\nno entablar alguna que otra amistad.");
-		txtrFdsfsd.setBackground(new Color(34, 40, 49));
-		txtrFdsfsd.setBounds(111, 244, 593, 113);
-		frame.getContentPane().add(txtrFdsfsd);
-		
-		btnNewButton_2 = new JButton("Editar perfil");
+		btnNewButton_2 = new JButton("Guardar");
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnNewButton_2.setForeground(Color.WHITE);
 		btnNewButton_2.setBackground(new Color(53, 187, 95));
@@ -203,16 +179,35 @@ public class Perfil {
 		btnNewButton_2.setBounds(642, 382, 133, 33);
 		frame.getContentPane().add(btnNewButton_2);
 		
-		btnNewButton_3 = new JButton("Historial");
-		btnNewButton_3.setForeground(Color.WHITE);
-		btnNewButton_3.setBorder(null);
-		btnNewButton_3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_3.setBackground(new Color(156, 163, 219));
-		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnNewButton_3.setBounds(74, 382, 133, 33);
-		frame.getContentPane().add(btnNewButton_3);
+		txtNuevaLocalidad = new JTextField();
+		txtNuevaLocalidad.setForeground(Color.LIGHT_GRAY);
+		txtNuevaLocalidad.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtNuevaLocalidad.setText("Nueva localidad...");
+		txtNuevaLocalidad.setBounds(111, 138, 145, 24);
+		frame.getContentPane().add(txtNuevaLocalidad);
+		txtNuevaLocalidad.setColumns(10);
+		
+		txtNuevoSexo = new JTextField();
+		txtNuevoSexo.setForeground(Color.LIGHT_GRAY);
+		txtNuevoSexo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtNuevoSexo.setText("Nuevo sexo...");
+		txtNuevoSexo.setBounds(111, 181, 145, 24);
+		frame.getContentPane().add(txtNuevoSexo);
+		txtNuevoSexo.setColumns(10);
+		
+		txtNuevoDeporte = new JTextField();
+		txtNuevoDeporte.setForeground(Color.LIGHT_GRAY);
+		txtNuevoDeporte.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtNuevoDeporte.setText("Nuevo deporte...");
+		txtNuevoDeporte.setBounds(336, 181, 145, 24);
+		frame.getContentPane().add(txtNuevoDeporte);
+		txtNuevoDeporte.setColumns(10);
+		
+		txtrModificarDescripcion = new JTextArea();
+		txtrModificarDescripcion.setForeground(Color.LIGHT_GRAY);
+		txtrModificarDescripcion.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtrModificarDescripcion.setText("Modificar descripcion...");
+		txtrModificarDescripcion.setBounds(111, 246, 550, 105);
+		frame.getContentPane().add(txtrModificarDescripcion);
 	}
 }
