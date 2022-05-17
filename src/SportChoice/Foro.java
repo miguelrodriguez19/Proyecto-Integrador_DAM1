b0824f3feece8c107caa0d15132c670ddaecaffa
IGNACIO MORENO FERNANDEZ <nacho5868@gmail.com>
2022-05-17 14:23:45 +0200

    FORO

--- src/SportChoice/Foro.java
+++ src/SportChoice/Foro.java
@@ -6,13 +6,14 @@ import java.awt.Cursor;
 import java.awt.EventQueue;
 
 import javax.swing.*;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class Foro extends JFrame{
 	private Controlador miControlador;
 	private Modelo miModelo;
-	private JTextField txtGenialGracias, txtHolaYoLlevo, txtElTemaDe, txtLlevamosCamisetasAzules, txtAlliNosVemos,
-			txtEscribeAqui, txtPartidoFutbolColegas;
-	private JButton btnPerfil1, btnPerfil2, btnPerfil3, btnPerfil4, btnPerfil5, btnEnviar;
+	private JTextField txtEscribeAqui;
+	private JButton btnPerfil1, btnPerfil2, btnPerfil3, btnPerfil4, btnEnviar;
 	private JPanel panel_1, panelFondo;
 
 	public static void Foro() {
@@ -42,17 +43,10 @@ public class Foro extends JFrame{
 
 		panel_1 = new JPanel();
 		panel_1.setBorder(null);
-		panel_1.setBackground(new Color(70, 130, 180));
+		panel_1.setBackground(new Color(0, 0, 128));
 		panel_1.setBounds(55, 10, 366, 39);
 		getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-
-		txtPartidoFutbolColegas = new JTextField();
-		txtPartidoFutbolColegas.setBounds(10, 10, 311, 19);
-		panel_1.add(txtPartidoFutbolColegas);
-		txtPartidoFutbolColegas.setBackground(new Color(240, 240, 240));
-		txtPartidoFutbolColegas.setText("PARTIDO FUTBOL COLEGAS");
-		txtPartidoFutbolColegas.setColumns(10);
 
 		JButton btnX = new JButton("X");
 		btnX.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -60,82 +54,128 @@ public class Foro extends JFrame{
 		panel_1.add(btnX);
 		btnX.setBorder(null);
 		btnX.setBackground(Color.WHITE);
+		
+		JLabel lblNewLabel_5 = new JLabel("PARTIDO FUTBOL COLEGAS");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblNewLabel_5.setForeground(Color.WHITE);
+		lblNewLabel_5.setBounds(10, 6, 302, 20);
+		panel_1.add(lblNewLabel_5);
 
 		panelFondo = new JPanel();
-		panelFondo.setBounds(55, 10, 364, 404);
+		panelFondo.setBounds(55, 10, 364, 423);
 		getContentPane().add(panelFondo);
 		panelFondo.setLayout(null);
 
-		txtHolaYoLlevo = new JTextField();
-		txtHolaYoLlevo.setText("Hola, yo llevo la pelota");
-		txtHolaYoLlevo.setBounds(69, 61, 141, 27);
-		panelFondo.add(txtHolaYoLlevo);
-		txtHolaYoLlevo.setColumns(10);
-
-		txtGenialGracias = new JTextField();
-		txtGenialGracias.setText("Genial, gracias!");
-		txtGenialGracias.setBounds(131, 114, 161, 27);
-		panelFondo.add(txtGenialGracias);
-		txtGenialGracias.setColumns(10);
-
-		txtElTemaDe = new JTextField();
-		txtElTemaDe.setText("El tema de las equipaciones?");
-		txtElTemaDe.setBounds(69, 166, 152, 27);
-		panelFondo.add(txtElTemaDe);
-		txtElTemaDe.setColumns(10);
-
-		txtLlevamosCamisetasAzules = new JTextField();
-		txtLlevamosCamisetasAzules.setText("Llevamos camisetas azules y blancas?");
-		txtLlevamosCamisetasAzules.setBounds(69, 222, 202, 27);
-		panelFondo.add(txtLlevamosCamisetasAzules);
-		txtLlevamosCamisetasAzules.setColumns(10);
-
-		txtAlliNosVemos = new JTextField();
-		txtAlliNosVemos.setText("Alli nos vemos");
-		txtAlliNosVemos.setBounds(190, 270, 103, 27);
-		panelFondo.add(txtAlliNosVemos);
-		txtAlliNosVemos.setColumns(10);
-
 		txtEscribeAqui = new JTextField();
 		txtEscribeAqui.setText("ESCRIBE AQUI...");
-		txtEscribeAqui.setBounds(10, 355, 249, 39);
+		txtEscribeAqui.setBounds(10, 374, 249, 39);
 		panelFondo.add(txtEscribeAqui);
 		txtEscribeAqui.setColumns(10);
 
 		btnEnviar = new JButton("ENVIAR");
 		btnEnviar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnEnviar.setBounds(269, 361, 85, 27);
+		btnEnviar.setBounds(269, 381, 85, 27);
 		panelFondo.add(btnEnviar);
 
 		btnPerfil1 = new JButton("");
 		btnPerfil1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnPerfil1.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
-		btnPerfil1.setBounds(10, 48, 52, 46);
+		btnPerfil1.setBounds(10, 50, 52, 46);
 		panelFondo.add(btnPerfil1);
 
 		btnPerfil2 = new JButton("");
 		btnPerfil2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnPerfil2.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
-		btnPerfil2.setBounds(302, 106, 52, 46);
+		btnPerfil2.setBounds(10, 120, 52, 46);
 		panelFondo.add(btnPerfil2);
-
-		btnPerfil3 = new JButton("");
-		btnPerfil3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnPerfil3.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
-		btnPerfil3.setBounds(10, 154, 52, 46);
-		panelFondo.add(btnPerfil3);
-
-		btnPerfil4 = new JButton("");
-		btnPerfil4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnPerfil4.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
-		btnPerfil4.setBounds(10, 213, 52, 46);
-		panelFondo.add(btnPerfil4);
-
-		btnPerfil5 = new JButton("");
-		btnPerfil5.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnPerfil5.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
-		btnPerfil5.setBounds(303, 261, 52, 46);
-		panelFondo.add(btnPerfil5);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.inactiveCaption);
+		panel.setBounds(0, 45, 364, 68);
+		panelFondo.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("18-05-2022 // 12:22");
+		lblNewLabel.setBounds(253, 45, 101, 13);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_6 = new JLabel("Hola, yo llevo la pelota");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_6.setBounds(84, 19, 202, 23);
+		panel.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("@dogue_drake");
+		lblNewLabel_7.setBounds(5, 52, 112, 13);
+		panel.add(lblNewLabel_7);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(SystemColor.controlHighlight);
+		panel_2.setBounds(0, 116, 364, 68);
+		panelFondo.add(panel_2);
+		panel_2.setLayout(null);
+				
+				JLabel lblNewLabel_6_1 = new JLabel("Genial, gracias!!");
+				lblNewLabel_6_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+				lblNewLabel_6_1.setBounds(84, 23, 120, 23);
+				panel_2.add(lblNewLabel_6_1);
+				
+				JLabel lblNewLabel_7_1 = new JLabel("@Juanito99");
+				lblNewLabel_7_1.setBounds(10, 52, 62, 13);
+				panel_2.add(lblNewLabel_7_1);
+				
+				JLabel lblNewLabel_1 = new JLabel("18-05-2022 // 12:35");
+				lblNewLabel_1.setBounds(253, 45, 94, 13);
+				panel_2.add(lblNewLabel_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(SystemColor.inactiveCaption);
+		panel_3.setBounds(0, 188, 364, 74);
+		panelFondo.add(panel_3);
+		panel_3.setLayout(null);
+		
+				btnPerfil3 = new JButton("");
+				btnPerfil3.setBounds(10, 7, 52, 46);
+				panel_3.add(btnPerfil3);
+				btnPerfil3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+				btnPerfil3.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
+						
+						JLabel lblNewLabel_2 = new JLabel("18-05-2022 // 13:10");
+						lblNewLabel_2.setBounds(253, 45, 101, 13);
+						panel_3.add(lblNewLabel_2);
+						
+						JLabel lblNewLabel_6_1_1 = new JLabel("El tema de las equipaciones?");
+						lblNewLabel_6_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+						lblNewLabel_6_1_1.setBounds(84, 18, 203, 23);
+						panel_3.add(lblNewLabel_6_1_1);
+						
+						JLabel lblNewLabel_7_1_1 = new JLabel("@Pedro_ppr");
+						lblNewLabel_7_1_1.setBounds(10, 57, 62, 13);
+						panel_3.add(lblNewLabel_7_1_1);
+		
+		JPanel panel_2_1 = new JPanel();
+		panel_2_1.setBackground(SystemColor.controlHighlight);
+		panel_2_1.setBounds(0, 268, 364, 74);
+		panelFondo.add(panel_2_1);
+		panel_2_1.setLayout(null);
+		
+				btnPerfil4 = new JButton("");
+				btnPerfil4.setBounds(10, 6, 52, 46);
+				panel_2_1.add(btnPerfil4);
+				btnPerfil4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+				btnPerfil4.setIcon(new ImageIcon(Foro.class.getResource("/Imagenes/usuario-de-perfil.png")));
+						
+						JLabel lblNewLabel_3 = new JLabel("18-05-2022  // 13:11");
+						lblNewLabel_3.setBounds(253, 45, 101, 13);
+						panel_2_1.add(lblNewLabel_3);
+						
+						JLabel lblNewLabel_6_1_1_1 = new JLabel("LLevamos camisetas azules y blancas");
+						lblNewLabel_6_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+						lblNewLabel_6_1_1_1.setBounds(84, 21, 259, 23);
+						panel_2_1.add(lblNewLabel_6_1_1_1);
+						
+						JLabel lblNewLabel_7_1_1_1 = new JLabel("@Maria889");
+						lblNewLabel_7_1_1_1.setBounds(10, 55, 62, 13);
+						panel_2_1.add(lblNewLabel_7_1_1_1);
 	}
 
 	
