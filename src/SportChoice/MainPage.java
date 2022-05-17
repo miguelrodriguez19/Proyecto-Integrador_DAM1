8a100bd71ba08bb4825c3a597958d9ec66c3ce13
IGNACIO MORENO FERNANDEZ <nacho5868@gmail.com>
2022-05-17 12:25:31 +0200

    Cambio

--- src/SportChoice/MainPage.java
+++ src/SportChoice/MainPage.java
@@ -112,7 +112,7 @@ public class MainPage extends JFrame{
 		comboBoxDia.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		comboBoxDia.setBackground(Color.WHITE);
 		comboBoxDia.setModel(new DefaultComboBoxModel(new String[] { "Dia", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-				"10", "11", "12", "13", "14", "15", "16" }));
+				"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
 		comboBoxDia.setBounds(29, 110, 59, 22);
 		panelPaginaPrincipal.add(comboBoxDia);
 
