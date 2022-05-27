a63573d539f3f7aae73c926b0f086380da05f1ad
Daniel Vázquez Pérez <danielvazquezperez5@gmail.com>
2022-05-27 11:00:11 +0200

    Mini arreglo desplegable puntos

--- src/SportChoice/Perfil.java
+++ src/SportChoice/Perfil.java
@@ -70,6 +70,7 @@ public class Perfil extends JFrame {
 		btnLogo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				miControlador.actualizar(10, 11);
+				esconderPuntitos();
 			}
 		});
 		header.add(btnLogo);
@@ -171,6 +172,7 @@ public class Perfil extends JFrame {
 		btnEditarPerfil.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				miControlador.actualizar(10, 3);
+				esconderPuntitos();
 			}
 		});
 
@@ -185,7 +187,7 @@ public class Perfil extends JFrame {
 		btnHistorial.setVisible(false);
 		btnHistorial.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				miControlador.actualizar(10, 6);
+				esconderPuntitos();
 			}
 		});
 
@@ -201,6 +203,7 @@ public class Perfil extends JFrame {
 		btnLogOut.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				miControlador.actualizar(10, 7);
+				esconderPuntitos();
 			}
 		});
 
@@ -212,7 +215,7 @@ public class Perfil extends JFrame {
 		btnFAQs.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnFAQs.setBounds(697, 247, 123, 33);
 		getContentPane().add(btnFAQs);
-		
+
 		lblPerfil_1 = new JLabel("Perfil");
 		lblPerfil_1.setForeground(Color.WHITE);
 		lblPerfil_1.setFont(new Font("Tahoma", Font.PLAIN, 28));
@@ -222,16 +225,14 @@ public class Perfil extends JFrame {
 		btnFAQs.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				miControlador.actualizar(10, 4);
+				esconderPuntitos();
 			}
 		});
 
 		btnPuntitos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (btnHistorial.isVisible() && btnEditarPerfil.isVisible() && btnLogOut.isVisible()) {
-					btnHistorial.setVisible(false);
-					btnEditarPerfil.setVisible(false);
-					btnFAQs.setVisible(false);
-					btnLogOut.setVisible(false);
+					esconderPuntitos();
 				} else {
 					btnHistorial.setVisible(true);
 					btnEditarPerfil.setVisible(true);
@@ -241,6 +242,13 @@ public class Perfil extends JFrame {
 
 			}
 		});
+	}
+
+	private void esconderPuntitos() {
+		btnHistorial.setVisible(false);
+		btnEditarPerfil.setVisible(false);
+		btnFAQs.setVisible(false);
+		btnLogOut.setVisible(false);
 	}
 
 	public void setMiControlador(Controlador miControlador) {
