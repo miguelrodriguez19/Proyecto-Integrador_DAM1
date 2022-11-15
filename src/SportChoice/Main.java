a38b2f4c1b5a6bb06d81e9e50507823fafd60925
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2022-11-15 09:35:57 +0100

    Final commit

--- src/SportChoice/Main.java
+++ src/SportChoice/Main.java
@@ -1,12 +1,6 @@
 package SportChoice;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.awt.event.ActionEvent;
 
 public class Main {
 	public static void main(String[] args) {
